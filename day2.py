12ce4f0eb21dedb60447e2383200bb9a4769ce50
Anna Lindeberg <anna@familjenlindeberg.com>
2022-12-02 11:29:01 +0100

    Clean day 2

--- day2.py
+++ day2.py
@@ -1,71 +1,46 @@
-# Day 2 of Advent of Code 2022:
+# Day 2 of Advent of Code 2022: Rock Paper Scissors
 # https://adventofcode.com/2022/day/2
 
 # A = X = rock = 1p
 # B = Y = paper = 2p
 # C = Z = scissors = 3p
 # loose 0p draw 3p win 6p
-
-hand_points = {'X' : 1, 'Y': 2, 'Z': 3}
+legend = {'X' : 'R', 'Y': 'P', 'Z': 'S', 'A' : 'R', 'B': 'P', 'C': 'S'}
+hand_points = {'R' : 1, 'P': 2, 'S': 3}
 
 def winner(hand1, hand2):
-    # True if hand2 wins
-    if hand1 == 'A' and hand2 == 'Y':
-        return True
-    elif hand1 == 'B' and hand2 == 'Z':
-        return True
-    elif hand1 == 'C' and hand2 == 'X':
-        return True
-    else:
-        return False
+    return hand1 == win_against(hand2)
 
 def draw(hand1, hand2):
-     if hand1 == 'A' and hand2 == 'X':
-         return True
-     elif hand1 == 'B' and hand2 == 'Y':
-         return True
-     elif hand1 == 'C' and hand2 == 'Z':
-         return True
-     else:
-         return False
+     return hand1 == hand2
 
 def win_against(hand):
-    idx = "ABCA".index(hand)
-    return "XYZX"[idx + 1]
+    return "RPS"["PSR".index(hand)]
 
 def loose_against(hand):
-    idx = "ABC".index(hand)
-    return "XYZ"[idx - 1]
+    return "SPR"["PRS".index(hand)]
 
 def draw_against(hand):
-    idx = "ABC".index(hand)
-    return "XYZ"[idx]
+    return hand
 
 def task1():
     tot = 0
     with open("input2.txt") as file:
       for row in file:
-          H1, H2 = row.strip().split()
-          if winner(H1, H2):
-              tot += 6 + hand_points[H2]
-          elif draw(H1, H2):
-              tot += 3 + hand_points[H2]
-          else:
-              tot += hand_points[H2]
-
+          H1, H2 = map(lambda x: legend[x], row.strip().split())
+          bonus = 6 if winner(H1, H2) else 3 if draw(H1,H2) else 0
+          tot += bonus + hand_points[H2]
     return tot
 
 def task2():
+    bonusPoints = {'X' : 0, 'Y': 3, 'Z': 6}
+    findHandBy = {'X' : win_against, 'Y': draw_against, 'Z': loose_against}
     tot = 0
     with open("input2.txt") as file:
       for row in file:
           H1, H2 = row.strip().split()
-          if H2 == 'X':
-              tot += hand_points[loose_against(H1)]
-          elif H2 == 'Y':
-              tot += 3 + hand_points[draw_against(H1)]
-          else:
-              tot += 6 + hand_points[win_against(H1)]
+          H1 = legend[H1]
+          tot += bonusPoints[H2] + hand_points[findHandBy[H2](H1)]
     return tot
 
-print(task2())
+print(f"Task 1:{task1()}\nTask 2:{task2()}")
