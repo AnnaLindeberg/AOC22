a0dd211c6d24ed3f08d59f598b17c304cb886f8e
Anna Lindeberg <anna@familjenlindeberg.com>
2022-12-09 12:08:52 +0100

    Day 8 solved

--- day8.py
+++ day8.py
@@ -1,6 +1,8 @@
 # Day 8 of Advent of Code 2022:
 # https://adventofcode.com/2022/day/8
-from operator import or_
+from functools import reduce
+from operator import mul
+
 
 def visibleLeft(heights):
     visibility = []
@@ -16,44 +18,82 @@ def visibleLeft(heights):
 
     return visibility
 
-def visibleWN(heights):
-    rows, cols = len(heights), len(heights[0])
-    # initialize visible trees from west and north
-    visibility = [[True] * cols] # + [[True] + [None] * (cols - 1)]*(rows - 1)
-    for _ in range(rows - 1):
-        visibility += [[True]]
 
-    for row in range(1, rows):
-        for col in range(1, cols):
-            # can tree at (row, col) be seen from north?
-            if visibility[row - 1][col] and heights[row - 1][col] < heights[row][col]:
-                visibility[row].append(True)
-            # or from west?
-            elif visibility[row][col - 1] and heights[row][col - 1] < heights[row][col]:
-                visibility[row].append(True)
+def reverseRows(LL):
+    return list(map(lambda l: list(reversed(l)), LL))
+
+
+def transpose(LL):
+    return [list(i) for i in zip(*LL)]
+
+
+def printVisibles(LL):
+    s = ''
+    for row in LL:
+        for el in row:
+            if el:
+                s += 'T'
             else:
-                visibility[row].append(False)
-    return visibility
+                s += 'F'
+        s += '\n'
+    print(s)
 
-def visibleES(heights):
-     mirrored = list(map(lambda l: list(reversed(l)), reversed(heights)))
-     res = visibleWN(mirrored)
-     return list(map(lambda l: list(reversed(l)), reversed(res)))
 
 def visible(heights):
-    WN = visibleWN(heights)
-    ES = visibleES(heights)
-    res = [list(map(or_, ES[i], WN[i])) for i in range(len(WN))]
+    left = visibleLeft(heights)
+    right = reverseRows(visibleLeft(reverseRows(heights)))
+    top = transpose(visibleLeft(transpose(heights)))
+    bottom = transpose(reverseRows(visibleLeft(reverseRows(transpose(heights)))))
+    res = [list(map(any, zip(left[i], right[i], top[i], bottom[i]))) for i in range(len(left))]
     return res
+
 
 def countVisible(visibles):
     return sum([el for row in visibles for el in row])
 
-forest = []
-with open("small_input8.txt") as file:
-    for row in file:
-        forest.append(list(map(int, row.strip())))
 
-print(visibleLeft(forest))
+# For task 2
 
-print(f"Task 1: {countVisible(visible(forest))}\nTask 2: {0}")
+def scenicScore(forest, tree):
+    res = []  # will be no of steps in resp direction [N, W, S, E]
+    treeHeight = forest[tree[0]][tree[1]]
+    for dir in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+        count = 0
+        x, y = tree
+        while True:
+            try:
+                x += dir[0]
+                y += dir[1]
+                if x < 0 or y < 0:
+                    break
+                nextTree = forest[x][y]
+                count += 1
+                if nextTree >= treeHeight:
+                    break
+            except IndexError:
+                break
+        res.append(count)
+    return reduce(mul, res, 1)
+
+
+def bestScenicScore(forest):
+    best = 0
+    for i in range(len(forest)):
+        for j in range(len(forest[0])):
+            score = scenicScore(forest, (i, j))
+            if score > best:
+                best = score
+    return best
+
+
+def main():
+    forest = []
+    with open("input8.txt") as file:
+        for row in file:
+            forest.append(list(map(int, row.strip())))
+
+    print(f"Task 1: {countVisible(visible(forest))}\nTask 2: {bestScenicScore(forest)}")
+
+
+if __name__ == main():
+    main()
